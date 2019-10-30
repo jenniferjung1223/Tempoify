54d72c9b71534dbf764d8283d8aba875409f61df
jenniferjung1223 <56170626+jenniferjung1223@users.noreply.github.com>
2019-10-29 22:44:44 -0500

    Add files via upload

---
+++ TemperatureScreen.py
@@ -0,0 +1,121 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+display_width = 800
+display_height =450 
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+
+pygame.display.set_caption('Tempoify')
+
+##icon = pygame.image.load("bookshelves.gif")
+##pygame.display.set_icon(icon)
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+red = (200, 0, 0)
+light_red = (255, 0, 0)
+
+yellow = (200, 200, 0)
+light_yellow = (255, 255, 0)
+
+green = (34, 177, 76)
+light_green = (0, 255, 0)
+
+clock = pygame.time.Clock()
+
+smallfont = pygame.font.SysFont("comicsansms", 25)
+medfont = pygame.font.SysFont("comicsansms", 50)
+largefont = pygame.font.SysFont("comicsansms", 75)
+
+
+##img = pygame.image.load('bookshelves.gif')
+# appleimg = pygame.image.load('apple.png')
+
+##def score(score):
+##
+##    text = smallfont.render("Score: "+str(score), True, black)
+##    gameDisplay.blit(text, [0,0])
+
+
+def text_objects(text, color, size="small"):
+    if size == "small":
+        textSurface = smallfont.render(text, True, color)
+    if size == "medium":
+        textSurface = medfont.render(text, True, color)
+    if size == "large":
+        textSurface = largefont.render(text, True, color)
+
+    return textSurface, textSurface.get_rect()
+
+
+def text_to_button(msg, color, buttonx, buttony, buttonwidth, buttonheight, size="small"):
+    textSurf, textRect = text_objects(msg, color, size)
+    textRect.center = round(buttonx + (buttonwidth / 2)), round(buttony + (buttonheight / 2))
+    gameDisplay.blit(textSurf, textRect)
+
+
+def message_to_screen(msg, color, y_displace=0, size="small"):
+    textSurf, textRect = text_objects(msg, color, size)
+    x = int(round((display_width / 2)))
+    y = int(round((display_height / 2) + y_displace))
+
+    textRect.center = x, y
+    gameDisplay.blit(textSurf, textRect)
+
+
+def pause():
+
+    paused = True
+    message_to_screen("Paused",black,-100,size="large")
+    message_to_screen("Press C to continue playing or Q to quit",black,25)
+    pygame.display.update()
+    while paused:
+        for event in pygame.event.get():
+                
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_c:
+                        paused = False
+                    elif event.key == pygame.K_q:
+                        pygame.quit()
+                        quit()
+
+        
+
+        clock.tick(5)
+
+
+def game_intro():
+    intro = True
+
+    while intro:
+        for event in pygame.event.get():
+            # print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_c:
+                    intro = False
+                elif event.key == pygame.K_q:
+
+                    pygame.quit()
+                    quit()
+
+        gameDisplay.fill(white)
+        message_to_screen("Let's start. First, touch the temperature sensor :)", black, 10)
+        message_to_screen("Don't be afraid, it won't kill you... yet *smile*", black, 50)
+
+        pygame.display.update()
+
+        clock.tick(15)
+
+game_intro()
